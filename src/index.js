5f8de18792b2de27387c82475e1c0d1961d18a37
dprandle <dprandle@alaska.edu>
2022-10-21 10:29:09 -0800

    WebSocket server running

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+const WebSocketServer = require('ws');
+const wss = new WebSocketServer.Server({ port: 3000 })
+
+var gsockets = [];
+
+// Creating connection using websocket
+wss.on("connection", ws => {
+    console.log("new client connected");
+    // sending message
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+    });
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
 
 app.get('/', function(req, res){
    res.sendFile(path.join(__dirname, 'emscripten', 'jackal_ctrl.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'emscripten')));
-
-app.listen(80);
+app.listen(8080);
