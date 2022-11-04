ea283be00a99fb66855e8fb62f01baf6b6cfbe8c
dprandle <dprandle@alaska.edu>
2022-11-04 10:12:22 -0800

    Add parsing for occupancy grid map

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const wss_lib = require('ws');
 const rosnodejs = require('rosnodejs');
 const geom_msgs = rosnodejs.require('geometry_msgs').msg;
-const sensor_msgs = rosnodejs.require('sensor_msgs').msg;
+// const sensor_msgs = rosnodejs.require('sensor_msgs').msg;
+// const nav_msgs = rosnodejs.require('nav_msgs').msg;
 
 const net = require('net');
 
@@ -17,8 +18,14 @@ const desktop_server_port = 4000;
 const http_server_port = 8080;
 const PACKET_STR_ID_LEN = 16;
 
+let debug_print = true;
+function dlog(params) {debug_print&&console.log(params);}
+
+let warning_print = true;
+function wlog(params) {warning_print&&console.log(params);}
+
 let pub = {};
-let scan_sent_count = 0;
+const occ_grid = [];
 
 function send_packet_to_clients(packet) {
     for (let i = 0; i < wsockets.length; ++i)
@@ -31,40 +38,136 @@ const scan_packet_header = {
     type: "SCAN_PCKT_ID\0\0\0\0"
 };
 
+const occ_grid_header = {
+    type: "OC_GRID_PCKT_ID\0"
+};
+
 const vel_cmd_header = {
     type: "VEL_CMD_PCKT_ID\0"
 };
 
 function write_packet_header(packet_header, packet) {
-    for (let i = 0; i < scan_packet_header.type.length; ++i)
-        packet.writeInt8(scan_packet_header.type.charCodeAt(i), i);
+    for (let i = 0; i < packet_header.type.length; ++i)
+        packet.writeInt8(packet_header.type.charCodeAt(i), i);
+}
+
+function update_occ_grid_and_get_delta(cm_data)
+{
+    dlog(`Occ grid cur size:${cm_data.length} prev size:${occ_grid.length}`);
+    let changes = [];
+    changes.length = 0;
+    for (let i = 0; i < cm_data.length; ++i)
+    {
+        if (!(i in occ_grid))
+        {
+            if (occ_grid.length != i)
+            {
+                wlog("The occ grid length is not what we expect");
+            }
+            occ_grid.push(cm_data[i]);
+            changes.push({ind: i, val: cm_data[i]})
+        }
+        else if (cm_data[i] !== occ_grid[i])
+        {
+            occ_grid[i] = cm_data[i];
+            changes.push({ind: i, val: cm_data[i]})
+        }
+    }
+    occ_grid.length = cm_data.length;
+    dlog(`Updated occ grid size:${occ_grid.length}`);
+    dlog(`Got ${changes.length} changes to be sent over the wire!`);
+    return changes;
+}
+
+
+function create_packet_from_costmap(cm) {
+    
+    // We don't need to send the entire costmap every update - instead just send the size of the costmap and changes
+    // since the last update - we can assume -1 (unknown) for all entries at start
+    const frame_changes = update_occ_grid_and_get_delta(cm.data)
+
+    if (frame_changes.length < 1)
+    {
+        dlog("Skipping sending packet - no changes found");
+    }
+
+    // Packet size is header plus map meta data (3 4 byte values) plus the origin pos and orientation
+    // (total of 7 doubles which are each 8 bytes), a single 4 byte value for the length of the changes array,
+    // and then the changes array where each entry is a 4 byte value - 3 MSB for the index and 1 LSB for the likelyhood value
+    const packet_size = PACKET_STR_ID_LEN + 4*3 + 8*7 + 4 + frame_changes.length * 4;
+
+    const packet = new Buffer.alloc(packet_size);
+    let offset = PACKET_STR_ID_LEN;
+
+    // Map meta data
+    write_packet_header(occ_grid_header, packet);
+    offset = packet.writeFloatLE(cm.info.resolution, offset);
+    offset = packet.writeUInt32LE(cm.info.width, offset);
+    offset = packet.writeUInt32LE(cm.info.height, offset);
+
+    // Origin position
+    offset = packet.writeDoubleLE(cm.info.origin.position.x, offset);
+    offset = packet.writeDoubleLE(cm.info.origin.position.y, offset);
+    offset = packet.writeDoubleLE(cm.info.origin.position.z, offset);
+
+    // Origin orientation
+    offset = packet.writeDoubleLE(cm.info.origin.orientation.x, offset);
+    offset = packet.writeDoubleLE(cm.info.origin.orientation.y, offset);
+    offset = packet.writeDoubleLE(cm.info.origin.orientation.z, offset);
+    offset = packet.writeDoubleLE(cm.info.origin.orientation.w, offset);
+
+    if (frame_changes.length > 16777216)
+        wlog("Changes array size is larger than allowed max");
+    
+    // Length of changes array
+    offset = packet.writeUInt32LE(frame_changes.length, offset);
+
+    for (let i = 0; i < frame_changes.length; ++i)
+    {
+        // Shift index to the left by one byte and use the LSByte for the likelihood value
+        // Since this is javascript, gotta manually check for -1 and convert to 255 to keep it within a byte
+        let byte_val = frame_changes[i].val;
+        if (byte_val == -1)
+            byte_val = 255;
+        
+        let val = (frame_changes[i].ind << 8) + byte_val;
+        offset = packet.writeInt32LE(val, offset);
+    }
 }
 
 function create_packet_from_scan(scan) {
     // 20 for the 5 floats listed below, and 4 bytes per scan range float
     const packet_size = PACKET_STR_ID_LEN + 20 + scan.ranges.length * 4;
     const packet = new Buffer.alloc(packet_size);
+    let offset = PACKET_STR_ID_LEN;
 
     write_packet_header(scan_packet_header, packet);
 
-    packet.writeFloatLE(scan.angle_min, PACKET_STR_ID_LEN);
-    packet.writeFloatLE(scan.angle_max, PACKET_STR_ID_LEN + 4);
-    packet.writeFloatLE(scan.angle_increment, PACKET_STR_ID_LEN + 8);
-    packet.writeFloatLE(scan.range_min, PACKET_STR_ID_LEN + 12);
-    packet.writeFloatLE(scan.range_max, PACKET_STR_ID_LEN + 16);
+    offset = packet.writeFloatLE(scan.angle_min, offset);
+    offset = packet.writeFloatLE(scan.angle_max, offset);
+    offset = packet.writeFloatLE(scan.angle_increment, offset);
+    offset = packet.writeFloatLE(scan.range_min, offset);
+    offset = packet.writeFloatLE(scan.range_max, offset);
     for (let i = 0; i < scan.ranges.length; ++i)
-        packet.writeFloatLE(scan.ranges[i], PACKET_STR_ID_LEN + 20 + 4 * i);
+        offset = packet.writeFloatLE(scan.ranges[i], offset);
     return packet;
 }
 
 // Create command server ROS node wich will relay our socket communications to control and monitor the robot
 rosnodejs.initNode("/command_server")
     .then((ros_node) => {
-        let sub = ros_node.subscribe("/front/scan", "sensor_msgs/LaserScan",
+        // Subscribe to the occupancy grid map messages - send packet with the map info
+        ros_node.subscribe("/map", "nav_msgs/OccupancyGrid",
+            (cm) => {
+                const packet = create_packet_from_costmap(cm);
+                send_packet_to_clients(packet);
+            });
+        
+        // Subscribe to the laser scan messages - send a packet with the scan info
+        ros_node.subscribe("/front/scan", "sensor_msgs/LaserScan",
             (scan) => {
                 const packet = create_packet_from_scan(scan);
                 send_packet_to_clients(packet);
-                ++scan_sent_count;
             });
         pub = rosnodejs.nh.advertise('/cmd_vel', 'geometry_msgs/Twist');
     });
