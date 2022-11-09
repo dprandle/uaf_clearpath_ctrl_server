889c9c8e37313730f2d03b43230e84751fda4beb
dprandle <dprandle@alaska.edu>
2022-11-08 15:29:15 -0900

    Forgot to commit last night - normal updated index thingy

--- src/index.js
+++ src/index.js
@@ -19,13 +19,34 @@ const http_server_port = 8080;
 const PACKET_STR_ID_LEN = 16;
 
 let debug_print = true;
-function dlog(params) {debug_print&&console.log(params);}
-
 let warning_print = true;
-function wlog(params) {warning_print&&console.log(params);}
+let info_print = true;
+function dlog(params) { debug_print && console.log(params); }
+function wlog(params) { warning_print && console.log(params); }
+function ilog(params) { info_print && console.log(params); }
 
 let pub = {};
-const occ_grid = [];
+const prev_frame_occ_grid = [];
+const connections_needing_full_occgrid = [];
+
+function get_element_index(value, array) {
+    return array.findIndex((element) => { return value === element });
+}
+
+function remove_element_at_index(ind, array)
+{
+    const last_ind = array.length - 1;
+    if (ind < 0)
+        return;
+
+    if (ind !== last_ind)
+        array[ind] = array[last_ind];
+    array.pop();
+}
+
+function remove_socket_from_array(sckt, array) {
+    remove_element_at_index(get_element_index(sckt, array), array);
+}
 
 function send_packet_to_clients(packet) {
     for (let i = 0; i < wsockets.length; ++i)
@@ -51,88 +72,75 @@ function write_packet_header(packet_header, packet) {
         packet.writeInt8(packet_header.type.charCodeAt(i), i);
 }
 
-function update_occ_grid_and_get_delta(cm_data)
-{
-    dlog(`Occ grid cur size:${cm_data.length} prev size:${occ_grid.length}`);
+function update_occ_grid_and_get_delta(cur_occ_grid, prev_occ_grid) {
+    dlog(`Occ grid cur size:${cur_occ_grid.length} prev size:${prev_occ_grid.length}`);
     let changes = [];
     changes.length = 0;
-    for (let i = 0; i < cm_data.length; ++i)
-    {
-        if (!(i in occ_grid))
-        {
-            if (occ_grid.length != i)
-            {
+    for (let i = 0; i < cur_occ_grid.length; ++i) {
+        if (!(i in prev_occ_grid)) {
+            if (prev_occ_grid.length != i) {
                 wlog("The occ grid length is not what we expect");
             }
-            occ_grid.push(cm_data[i]);
-            changes.push({ind: i, val: cm_data[i]})
+            prev_occ_grid.push(cur_occ_grid[i]);
+            changes.push({ ind: i, val: cur_occ_grid[i] })
         }
-        else if (cm_data[i] !== occ_grid[i])
-        {
-            occ_grid[i] = cm_data[i];
-            changes.push({ind: i, val: cm_data[i]})
+        else if (cur_occ_grid[i] !== prev_occ_grid[i]) {
+            prev_occ_grid[i] = cur_occ_grid[i];
+            changes.push({ ind: i, val: cur_occ_grid[i] })
         }
     }
-    occ_grid.length = cm_data.length;
-    dlog(`Updated occ grid size:${occ_grid.length}`);
-    dlog(`Got ${changes.length} changes to be sent over the wire!`);
+    prev_occ_grid.length = cur_occ_grid.length;
+    dlog(`Got ${changes.length} changes this frame!`);
     return changes;
 }
 
-
-function create_packet_from_costmap(cm) {
-    
-    // We don't need to send the entire costmap every update - instead just send the size of the costmap and changes
-    // since the last update - we can assume -1 (unknown) for all entries at start
-    const frame_changes = update_occ_grid_and_get_delta(cm.data)
-
-    if (frame_changes.length < 1)
-    {
-        dlog("Skipping sending packet - no changes found");
-    }
-
+function create_packet_from_occgrid(occ_grid_msg, frame_changes) {
     // Packet size is header plus map meta data (3 4 byte values) plus the origin pos and orientation
     // (total of 7 doubles which are each 8 bytes), a single 4 byte value for the length of the changes array,
     // and then the changes array where each entry is a 4 byte value - 3 MSB for the index and 1 LSB for the likelyhood value
-    const packet_size = PACKET_STR_ID_LEN + 4*3 + 8*7 + 4 + frame_changes.length * 4;
+    const packet_size = PACKET_STR_ID_LEN + 4 * 3 + 8 * 7 + 4 + frame_changes.length * 4;
 
     const packet = new Buffer.alloc(packet_size);
     let offset = PACKET_STR_ID_LEN;
 
     // Map meta data
     write_packet_header(occ_grid_header, packet);
-    offset = packet.writeFloatLE(cm.info.resolution, offset);
-    offset = packet.writeUInt32LE(cm.info.width, offset);
-    offset = packet.writeUInt32LE(cm.info.height, offset);
 
+    offset = packet.writeFloatLE(occ_grid_msg.info.resolution, offset);
+    offset = packet.writeUInt32LE(occ_grid_msg.info.width, offset);
+    offset = packet.writeUInt32LE(occ_grid_msg.info.height, offset);
+    
     // Origin position
-    offset = packet.writeDoubleLE(cm.info.origin.position.x, offset);
-    offset = packet.writeDoubleLE(cm.info.origin.position.y, offset);
-    offset = packet.writeDoubleLE(cm.info.origin.position.z, offset);
+    offset = packet.writeDoubleLE(occ_grid_msg.info.origin.position.x, offset);
+    offset = packet.writeDoubleLE(occ_grid_msg.info.origin.position.y, offset);
+    offset = packet.writeDoubleLE(occ_grid_msg.info.origin.position.z, offset);
 
     // Origin orientation
-    offset = packet.writeDoubleLE(cm.info.origin.orientation.x, offset);
-    offset = packet.writeDoubleLE(cm.info.origin.orientation.y, offset);
-    offset = packet.writeDoubleLE(cm.info.origin.orientation.z, offset);
-    offset = packet.writeDoubleLE(cm.info.origin.orientation.w, offset);
+    offset = packet.writeDoubleLE(occ_grid_msg.info.origin.orientation.x, offset);
+    offset = packet.writeDoubleLE(occ_grid_msg.info.origin.orientation.y, offset);
+    offset = packet.writeDoubleLE(occ_grid_msg.info.origin.orientation.z, offset);
+    offset = packet.writeDoubleLE(occ_grid_msg.info.origin.orientation.w, offset);
 
     if (frame_changes.length > 16777216)
         wlog("Changes array size is larger than allowed max");
     
-    // Length of changes array
+        // Length of changes array
+    dlog(`Sending frame_changes length ${frame_changes.length}`);
     offset = packet.writeUInt32LE(frame_changes.length, offset);
 
-    for (let i = 0; i < frame_changes.length; ++i)
-    {
+    dlog(`Sending message info ${JSON.stringify(occ_grid_msg.info)}`);
+
+    for (let i = 0; i < frame_changes.length; ++i) {
         // Shift index to the left by one byte and use the LSByte for the likelihood value
         // Since this is javascript, gotta manually check for -1 and convert to 255 to keep it within a byte
         let byte_val = frame_changes[i].val;
         if (byte_val == -1)
             byte_val = 255;
-        
+
         let val = (frame_changes[i].ind << 8) + byte_val;
-        offset = packet.writeInt32LE(val, offset);
+        offset = packet.writeUInt32LE(val, offset);
     }
+    return packet;
 }
 
 function create_packet_from_scan(scan) {
@@ -153,16 +161,72 @@ function create_packet_from_scan(scan) {
     return packet;
 }
 
+function parse_command_header(buf) {
+    return buf.toString('utf8', 0, PACKET_STR_ID_LEN);
+}
+
+function parse_vel_cmd(buf) {
+    return {
+        linear: buf.readFloatLE(PACKET_STR_ID_LEN),
+        angular: buf.readFloatLE(PACKET_STR_ID_LEN + 4)
+    };
+}
+
+function send_occ_grid_to_all_clients(socket_arr, normal_frame_packet, full_frame_packet, func_to_use_for_send)
+{
+    for (let i = 0; i < socket_arr.length; ++i) {
+        let packet_to_send = normal_frame_packet;
+
+        // If the socket is a new connection
+        let ind = get_element_index(socket_arr[i], connections_needing_full_occgrid);
+        if (ind >= 0)
+        {
+            dlog(`New connection sending full frame)`);
+            packet_to_send = full_frame_packet;
+            remove_element_at_index(ind, connections_needing_full_occgrid);
+        }
+        dlog(`Sending occ grid packet (${packet_to_send.length} bytes)`);
+        func_to_use_for_send(socket_arr[i], packet_to_send);
+    }
+}
+
+function send_occ_grid_to_clients(occ_grid_msg) {
+    const total_connections = wsockets.length + dt_sockets.length;
+    dlog(`Total connection count ${total_connections} and new connection count ${connections_needing_full_occgrid.length}`);
+
+    // If there is a newly established connection/s, we need to make a packet for them that has the whole costmap
+    let full_frame_packet = null;
+    if (connections_needing_full_occgrid.length > 0)
+    {
+        const frame_all_changes = update_occ_grid_and_get_delta(occ_grid_msg.data, []);
+        full_frame_packet = create_packet_from_occgrid(occ_grid_msg, frame_all_changes);
+    }
+
+    // We don't need to send the entire costmap every update - instead just send the size of the costmap and changes
+    // since the last update (for new connections - the above is needed as we can't just send the delta)
+    const frame_changes = update_occ_grid_and_get_delta(occ_grid_msg.data, prev_frame_occ_grid);
+    
+    // Only create the packet if it will be sent to someone
+    let normal_frame_packet = null;    
+    //dlog(`Normal frame packet ${normal_frame_packet}`);
+    if (total_connections > connections_needing_full_occgrid.length)
+    {
+        normal_frame_packet = create_packet_from_occgrid(occ_grid_msg, frame_changes);
+    }
+
+    send_occ_grid_to_all_clients(wsockets, normal_frame_packet, full_frame_packet, (sckt, pckt) => {sckt.send(pckt);});
+    send_occ_grid_to_all_clients(dt_sockets, normal_frame_packet, full_frame_packet, (sckt, pckt) => {sckt.write(pckt);});
+}
+
 // Create command server ROS node wich will relay our socket communications to control and monitor the robot
 rosnodejs.initNode("/command_server")
     .then((ros_node) => {
         // Subscribe to the occupancy grid map messages - send packet with the map info
         ros_node.subscribe("/map", "nav_msgs/OccupancyGrid",
-            (cm) => {
-                const packet = create_packet_from_costmap(cm);
-                send_packet_to_clients(packet);
+            (occ_grid_msg) => {
+                send_occ_grid_to_clients(occ_grid_msg);
             });
-        
+
         // Subscribe to the laser scan messages - send a packet with the scan info
         ros_node.subscribe("/front/scan", "sensor_msgs/LaserScan",
             (scan) => {
@@ -172,11 +236,8 @@ rosnodejs.initNode("/command_server")
         pub = rosnodejs.nh.advertise('/cmd_vel', 'geometry_msgs/Twist');
     });
 
-
 // Serve to browsers via websockets
 wss.on("connection", web_sckt => {
-    console.log("New client connected for web sockets");
-
     // sending message
     web_sckt.on("message", data => {
         const hdr = parse_command_header(data);
@@ -190,34 +251,27 @@ wss.on("connection", web_sckt => {
     });
 
     // handling what to do when clients disconnects from server
-    web_sckt.on("close", () => {
-        console.log("the client has disconnected");
+    web_sckt.on("close", (code, reason) => {
+        remove_socket_from_array(web_sckt, wsockets);
+
+        // Just in case a connection was added/removed super quickly
+        remove_socket_from_array(web_sckt, connections_needing_full_occgrid);
+        ilog(`Connection to ws client closed with code ${code} for reason ${reason} - ${wsockets.length} ws clients remain connected`);
     });
 
     // handling client connection error
-    web_sckt.onerror = function () {
-        console.log("Some Error occurred")
-    }
+    web_sckt.on("error", (error) => {
+        wlog(`An error on a web socket has occured: ${error}`);
+    });
 
     wsockets.push(web_sckt);
+    connections_needing_full_occgrid.push(web_sckt);
+    ilog(`New client connected for web sockets - ${wsockets.length} web sockets connected`);
+
 });
-
-
-function parse_command_header(buf) {
-    return buf.toString('utf8', 0, PACKET_STR_ID_LEN);
-}
-
-function parse_vel_cmd(buf) {
-    return {
-        linear: buf.readFloatLE(PACKET_STR_ID_LEN),
-        angular: buf.readFloatLE(PACKET_STR_ID_LEN + 4)
-    };
-}
-
 
 // Serve to desktop/smartphone clients - these are not web sockets
 non_browser_server.on("connection", (dt_skt) => {
-    console.log("Got connection from " + dt_skt.remoteAddress + " on port " + dt_skt.remotePort);
     // sending message
     dt_skt.on("data", data => {
         const hdr = parse_command_header(data);
@@ -231,24 +285,31 @@ non_browser_server.on("connection", (dt_skt) => {
     });
 
     dt_skt.on("error", err => {
-        console.log("Connection with non browser client " + dt_skt.remoteAddress + " encountered error: " + err);
+        ilog(`Connection to non ws client ${dt_skt.remoteAddress} encountered error: ${err}`);
     });
 
     // handling what to do when clients disconnects from server
     dt_skt.on("close", () => {
-        console.log("Connection to non browser " + dt_skt.remoteAddress + " was closed");
+        remove_socket_from_array(dt_skt, dt_sockets);
+
+        // Just in case a connection was added/removed super quickly
+        remove_socket_from_array(dt_skt, connections_needing_full_occgrid);
+
+        ilog(`Connection to non ws client ${dt_skt.remoteAddress}:${dt_skt.remotePort} was closed - ${dt_sockets.length} non ws clients remain connected`);
     });
 
     dt_sockets.push(dt_skt);
+    connections_needing_full_occgrid.push(dt_skt);
+    ilog(`Client connected from ${dt_skt.remoteAddress}:${dt_skt.remotePort} - ${dt_sockets.length} non ws clients connected`);
 
 });
 
 non_browser_server.on("error", (err) => {
-    console.log("Non browser server on " + non_browser_server.address() + " encountered error: " + err);
+    wlog(`Non browser server on ${non_browser_server.address()} encountered error: ${err}`);
 });
 
 non_browser_server.on("listening", () => {
-    console.log("Non browser server listening on " + non_browser_server.address());
+    ilog(`Non browser server listening on ${non_browser_server.address()}`);
 })
 
 // Serve up the emscripten generated main page
@@ -262,6 +323,7 @@ app.use(express.static(path.join(__dirname, 'emscripten')));
 // List on the normal http server - and when an upgrade request comes through (for websockets protocol), then route the
 // request to the above websocket handler
 const main_server = app.listen(http_server_port);
+
 main_server.on('upgrade', (request, socket, head) => {
     wss.handleUpgrade(request, socket, head, socket => {
         wss.emit('connection', socket, request);
